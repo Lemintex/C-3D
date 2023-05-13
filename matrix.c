edab022df5db97c75b115f7fda57dc85efbe87e3
Lemintex <aaron_nobile@hotmail.co.uk>
2023-05-14 00:40:17 +0100

    reformated matrix multiplication function

--- matrix.c
+++ matrix.c
@@ -67,7 +67,10 @@ matrix_4x4_t matrix_multiplyMatrix(matrix_4x4_t* m1, matrix_4x4_t* m2) {
 	matrix_4x4_t matrix = {0};
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
-			matrix.m[i][j] = m1->m[i][0] * m2->m[0][j] + m1->m[i][1] * m2->m[1][j] + m1->m[i][2] * m2->m[2][j] + m1->m[i][3] * m2->m[3][j];
+		    matrix.m[i][j] = 0;
+            for (int k = 0; k < 4; k++) {
+                matrix.m[i][j] += m1->m[i][k] * m2->m[k][j];
+            }
 		}
 	}
 	return matrix;
