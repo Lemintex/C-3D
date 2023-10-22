5ff58886f7503c36a6cb72e16e562f3359a2af3b
Lemintex <aaron_nobile@hotmail.co.uk>
2023-10-22 15:23:51 +0100

    removed method definitions from controller.h

--- src/controller.h
+++ src/controller.h
@@ -5,14 +5,4 @@
 
 void handleKeyboardInput(SDL_Event *type, unsigned short *mov);
 
-int getBit(unsigned short *keystate, int n);
-
-void onKeyPressed(SDL_Keycode *key);
-
-void onKeyReleased(SDL_Keycode *key);
-
-void handleMouseInput();
-
-void keyboard_flipBit(unsigned short *keyState);
-
 #endif
