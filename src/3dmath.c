d6d76a9a977dcd4ac4bce34b6cfd9181ddb8be9c
Lemintex <aaron_nobile@hotmail.co.uk>
2023-11-21 20:22:13 +0000

    Updated indentation

--- src/3dmath.c
+++ src/3dmath.c
@@ -39,6 +39,7 @@ vec3d_t vec3_normal(vec3d_t *v)
 vec3d_t vec3_cross(vec3d_t *v1, vec3d_t *v2)
 {
 	vec3d_t vec;
+
 	vec.x = v1->y * v2->z - v1->z * v2->y;
 	vec.y = v1->z * v2->x - v1->x * v2->z;
 	vec.z = v1->x * v2->y - v1->y * v2->x;
@@ -49,6 +50,7 @@ vec3d_t vec3_cross(vec3d_t *v1, vec3d_t *v2)
 vec3d_t vec3_mul_mat4(vec3d_t *v, matrix_4x4_t *m)
 {
 	vec3d_t vec;
+
 	vec.x = v->x * m->m[0][0] + v->y * m->m[1][0] + v->z * m->m[2][0] + v->w * m->m[3][0];
 	vec.y = v->x * m->m[0][1] + v->y * m->m[1][1] + v->z * m->m[2][1] + v->w * m->m[3][1];
 	vec.z = v->x * m->m[0][2] + v->y * m->m[1][2] + v->z * m->m[2][2] + v->w * m->m[3][2];
@@ -83,6 +85,7 @@ vec3d_t vec3_intersectPlane(vec3d_t *planePoint, vec3d_t *planeNormal,
 triangle_t triangle_mul_mat4(triangle_t *t, matrix_4x4_t *m)
 {
 	triangle_t triangle;
+
 	t->verts[0] = vec3_mul_mat4(&t->verts[0], m);
 	t->verts[1] = vec3_mul_mat4(&t->verts[1], m);
 	t->verts[2] = vec3_mul_mat4(&t->verts[2], m);
